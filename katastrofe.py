77f39f14b59377c6a1399ca3db75d0f38827e055
Yusufcan234334 <yusufcanonalan22@gmail.com>
2026-08-10 14:33:21 +0300

    biraz temizlik

---
+++ katastrofe.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as siniragi
+
+class muhtisimmodel(siniragi.Module):
+    def __init__(self, giris, cikis, katmansayisi, branchsayisi):
+        super().__init__()
+
+        self.branchler = siniragi.ModuleList()
+        self.fusion_weights = siniragi.Parameter(torch.ones(branchsayisi))
+        self.residual_weight = siniragi.Parameter(torch.tensor(1.0))
+
+        for branch in range(branchsayisi):
+            katmanlar = siniragi.ModuleList()
+
+            neuroncountin = giris
+            neuroncountout = cikis
+
+            for i in range(katmansayisi):
+                katmanlar.append(
+                    siniragi.Linear(neuroncountin, neuroncountout)
+                )
+
+                if i == katmansayisi - 1:
+                    break
+                else:
+                    neuroncountin = neuroncountout
+                    neuroncountout = neuroncountout * 2
+
+
+            for i in range(katmansayisi):
+                if i == katmansayisi - 1:
+                    neuroncountin = neuroncountout
+                    neuroncountout //= 2
+                    neuroncountoutson = neuroncountout
+                else:
+                    neuroncountin = neuroncountout
+                    neuroncountout //= 2
+
+                katmanlar.append(
+                    siniragi.Linear(neuroncountin, neuroncountout)
+                )
+
+            self.branchler.append(katmanlar)
+
+        self.attention = siniragi.MultiheadAttention(
+            embed_dim=neuroncountoutson,
+            num_heads=4,
+            batch_first=True
+        )
+
+        self.output1 = siniragi.Linear(
+            neuroncountoutson,
+            1
+        )
+
+    def forward(self, x):
+        ilkx = x
+        branchciktilari = []
+
+        for katmanlar in self.branchler:
+            x = ilkx
+
+            for katmannum, katman in enumerate(katmanlar):
+                x = katman(x)
+
+                if katmannum != len(katmanlar) - 1:
+                    x = torch.relu(x)
+
+            branchciktilari.append(x)
+
+        branchciktilari = torch.stack(branchciktilari,dim=1)
+        attended, attentionweights = self.attention(branchciktilari,branchciktilari,branchciktilari)
+        weights = torch.softmax(self.fusion_weights,dim=0)
+        fused = (attended+ self.residual_weight * branchciktilari)
+        fused = fused * weights.view(1, -1, 1)
+        fused = fused.sum(dim=1)
+        fused = self.output1(fused)
+        return fused
